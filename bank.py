cba1d2bb21e136655d074854eb4b098d38224512
neelavani-keerti <keertineelavani@gmail.com>
2026-01-08 16:02:30 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,13 +1,16 @@
-def bank_details(name, acc_no, acc_type):
-    return (
-        f"Account Name: {name}\n"
-        f"Account No: {acc_no}\n"
-        f"Account Type: {acc_type}"
+def bank_details(name,acc_no,acc_type):
+    result = (
+        f"Account Name:{name}\n"
+        f"Account No:{acc_no}\n"
+        f"Account Type:{acc_type}\n"
+      
     )
+    return result
 
 if __name__ == "__main__":
-    name = input("Enter account holder name: ")
-    acc_no = input("Enter account number: ")
-    acc_type = input("Enter account type: ")
+    name ="swapna"
+    acc_no ="1001"
+    acc_type ="current"
+   
 
-    print(bank_details(name, acc_no, acc_type))
+    print(bank_details(name,acc_no,acc_type))
